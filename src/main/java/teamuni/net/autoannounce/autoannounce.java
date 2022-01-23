8b66f8e529d6d0cfd032726c7f7903ba531f75d1
Fade Jung <Fade@teamuni.net>
2022-01-23 15:34:36 +0900

    공지에 색깔 넣을 수 있게하기.

--- src/main/java/teamuni/net/autoannounce/autoannounce.java
+++ src/main/java/teamuni/net/autoannounce/autoannounce.java
@@ -5,6 +5,7 @@ import java.lang.String;
 import java.util.Arrays;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -30,7 +31,30 @@ public final class autoannounce extends JavaPlugin {
         runnable = new BukkitRunnable() {
             @Override
             public void run() {
-                Bukkit.broadcastMessage(msg.replace("\\n", "\n"));
+                String msgSplit = (msg.replace("\\n","\n"));
+                String msgBlack = (msgSplit.replace("&0", ChatColor.BLACK + ""));
+                String msgDarkBlue = (msgBlack.replace("&1", ChatColor.DARK_BLUE + ""));
+                String msgDarkGreen = (msgDarkBlue.replace("&2", ChatColor.DARK_GREEN + ""));
+                String msgDarkAqua = (msgDarkGreen.replace("&3", ChatColor.DARK_AQUA + ""));
+                String msgDarkRed = (msgDarkAqua.replace("&4", ChatColor.DARK_RED + ""));
+                String msgDarkPurple = (msgDarkRed.replace("&5", ChatColor.DARK_PURPLE + ""));
+                String msgGold = (msgDarkPurple.replace("&6", ChatColor.GOLD + ""));
+                String msgGray = (msgGold.replace("&7", ChatColor.GRAY + ""));
+                String msgDarkGray = (msgGray.replace("&8", ChatColor.DARK_GRAY + ""));
+                String msgBlue = (msgDarkGray.replace("&9", ChatColor.BLUE + ""));
+                String msgGreen = (msgBlue.replace("&a", ChatColor.GREEN + ""));
+                String msgAqua = (msgGreen.replace("&b", ChatColor.AQUA + ""));
+                String msgRed = (msgAqua.replace("&c", ChatColor.RED + ""));
+                String msgLightPurple = (msgRed.replace("&d", ChatColor.LIGHT_PURPLE + ""));
+                String msgYellow = (msgLightPurple.replace("&e", ChatColor.YELLOW + ""));
+                String msgWhite = (msgYellow.replace("&f", ChatColor.WHITE + ""));
+                String msgMagic = (msgWhite.replace("&k", ChatColor.MAGIC + ""));
+                String msgBold = (msgMagic.replace("&l", ChatColor.BOLD + ""));
+                String msgStrikeThrough = (msgBold.replace("&m", ChatColor.STRIKETHROUGH + ""));
+                String msgUnderLine = (msgStrikeThrough.replace("&n", ChatColor.UNDERLINE + ""));
+                String msgItalic = (msgUnderLine.replace("&o", ChatColor.ITALIC + ""));
+                String msgReset = (msgItalic.replace("&r", ChatColor.RESET + ""));
+                Bukkit.broadcastMessage(msgReset);
             }
         };
         runnable.runTaskTimer(this, delay, delay);
@@ -74,7 +98,7 @@ public final class autoannounce extends JavaPlugin {
             }
             default: {
                 player.sendMessage("/autoannounce delay [딜레이] - 딜레이를 설정합니다. [20 = 1초]");
-                player.sendMessage("/autoannounce message [공지] - 공지를 수정합니다. [줄바꿈 \n ]");
+                player.sendMessage("/autoannounce message [공지] - 공지를 수정합니다.");
                 break;
             }
         }
