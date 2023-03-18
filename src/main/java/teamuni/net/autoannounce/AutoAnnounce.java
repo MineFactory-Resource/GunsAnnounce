cf334252b817cbe2c6d7477cde8d742782a24b8e
Seokhun-Yang <ddayang1234@gmail.com>
2023-03-18 19:50:10 +0900

    messages.yml 추가, 랜덤 출력 여부를 config.yml에서 설정할 수 있도록 하는 기능 추가

---
+++ src/main/java/teamuni/net/autoannounce/AutoAnnounce.java
@@ -0,0 +1,97 @@
+package teamuni.net.autoannounce;
+
+
+import java.lang.String;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoAnnounce extends JavaPlugin {
+    private MessageManager messageManager;
+    private boolean isUseRandom;
+    private long delay;
+    private long period;
+    private int num = 0;
+    private final Map<String, List<String>> messageMap = new HashMap<>();
+
+    public void onEnable() {
+        saveDefaultConfig();
+        this.messageManager = new MessageManager(this);
+        this.messageManager.createMessagesYml();
+        this.delay = getConfig().getLong("delay");
+        this.period = getConfig().getLong("period");
+        this.messageMap.putAll(this.messageManager.getMessages());
+        this.isUseRandom = getConfig().getBoolean("print_random");
+        registerTask();
+        getCommand("autoannounce").setTabCompleter(new CommandTabCompleter());
+    }
+
+    private void registerTask() {
+        List<String> tipList = this.messageMap.get("announce_messages");
+
+        if (tipList.size() == 0) {
+            return;
+        }
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            if (this.isUseRandom) {
+                Random random = new Random();
+                int randomNumber = random.nextInt(tipList.size());
+                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', tipList.get(randomNumber)));
+            } else {
+                if (this.num == tipList.size() - 1) {
+                    this.num = 0;
+                }
+                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', tipList.get(this.num)));
+                this.num++;
+            }
+        }, delay, period);
+    }
+
+
+    @Override
+    public void onDisable() {
+        saveConfig();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        if (sender instanceof Player player) {
+            if (command.getName().equalsIgnoreCase("autoannounce") && player.hasPermission("autoannounce.reload")) {
+                switch (args[0]) {
+                    case "reload" -> {
+                        reloadConfig();
+                        this.delay = getConfig().getLong("delay");
+                        this.period = getConfig().getLong("period");
+                        this.isUseRandom = getConfig().getBoolean("print_random");
+                        this.messageManager.reload();
+                        this.messageMap.putAll(this.messageManager.getMessages());
+                        this.messageManager.sendTranslatedMessage(player, this.messageMap.get("reload_message"));
+                    }
+                    case "help" ->
+                            this.messageManager.sendTranslatedMessage(player, this.messageMap.get("guide_message"));
+                    case "period" -> {
+                        if (!args[1].matches("[0-9]+")) {
+                            this.messageManager.sendTranslatedMessage(player, this.messageMap.get("syntax_error_message"));
+                            return false;
+                        }
+                        long i = Long.parseLong(args[1]);
+                        getConfig().set("period", i);
+                        saveConfig();
+                        this.period = i;
+                        this.messageManager.sendTranslatedMessage(player, this.messageMap.get("set_period_message"));
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/teamuni/net/autoannounce/MessageManager.java
@@ -0,0 +1,54 @@
+package teamuni.net.autoannounce;
+
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MessageManager {
+    private final AutoAnnounce instance;
+    private File file = null;
+    private FileConfiguration messagesFile = null;
+
+    public MessageManager(AutoAnnounce instance) {
+        this.instance = instance;
+    }
+
+    public void createMessagesYml() {
+        if (this.file == null) {
+            this.file = new File(this.instance.getDataFolder(), "messages.yml");
+        }
+        if (!file.exists()) {
+            this.instance.saveResource("messages.yml", false);
+        }
+        this.messagesFile = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public void reload() {
+        if (this.file == null) {
+            this.file = new File(this.instance.getDataFolder(), "messages.yml");
+        }
+        this.messagesFile = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public Map<String, List<String>> getMessages() {
+        Map<String, List<String>> messageListMap = new HashMap<>();
+
+        for (String key : this.messagesFile.getKeys(false)) {
+            List<String> messages = this.messagesFile.getStringList(key);
+            messageListMap.put(key, messages);
+        }
+        return messageListMap;
+    }
+
+    public void sendTranslatedMessage(Player player, List<String> msgList) {
+        for (String msg : msgList) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+        }
+    }
+}
